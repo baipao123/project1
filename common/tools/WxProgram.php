da2869cc552b93c13eadc0a3cb4a21ff5309174e
huangchen <772965632@qq.com>
2018-04-12 15:48:53 +0800

    小程序功能 获取用户openId、解密

---
+++ common/tools/WxProgram.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018-04-12
+ * Time: 15:07:15
+ */
+
+namespace common\tools;
+
+use Yii;
+
+class WxProgram
+{
+    const OK = 0;
+    const IllegalAesKey = -41001;
+    const IllegalIv = -41002;
+    const IllegalBuffer = -41003;
+    const DecodeBase64Error = -41004;
+
+    protected static function getAppId() {
+        return Yii::$app->params['appId'];
+    }
+
+    protected static function getAppSecret() {
+        return Yii::$app->params['appSecret'];
+    }
+
+    public static function getOpenId($code) {
+        $url = "https://api.weixin.qq.com/sns/jscode2session";
+        $params = [
+            "appid"      => self::getAppId(),
+            "secret"     => self::getAppSecret(),
+            "js_code"    => $code,
+            "grant_type" => "authorization_code"
+        ];
+
+        return [
+            "openId"      => "",
+            "session_key" => ""
+        ];
+    }
+
+    public static function decryptData($encryptedData, $iv, $sessionKey, &$data) {
+        if (strlen($sessionKey) != 24)
+            return self::IllegalAesKey;
+        $aesKey = base64_decode($sessionKey);
+        if (strlen($iv) != 24)
+            return self::IllegalIv;
+        $aesIV = base64_decode($iv);
+        $aesCipher = base64_decode($encryptedData);
+        $result = openssl_decrypt($aesCipher, "AES-128-CBC", $aesKey, 1, $aesIV);
+        $dataObj = json_decode($result);
+        if ($dataObj == NULL)
+            return self::IllegalBuffer;
+        if ($dataObj->watermark->appid != self::getAppId())
+            return self::IllegalBuffer;
+        $data = $result;
+        return self::OK;
+    }
+}
