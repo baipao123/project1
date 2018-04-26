36af7bdbc721bafb3e01a0239403e3d4e84beadc
baipao123 <huangchen@huangchendeMac-mini.local>
2018-04-26 22:26:16 +0800

    .

--- wxApp/pages/chose/company.js
+++ wxApp/pages/chose/company.js
@@ -8,8 +8,10 @@ Page({
         company: {},
         type: 0,
         domain: app.globalData.qiNiuDomain,
-        phone: "",
         isAgree: false,
+        images: [],
+        imageKeys: [],
+        deleteImage: false,
     },
     onLoad: function () {
         this.setData({
@@ -41,21 +43,22 @@ Page({
         let that = this;
         wx.chooseLocation({
             success: function (data) {
-                let company = that.data.company;
+                let company = that.data.company,
+                    obj = {
+                        "company.position": data
+                    };
                 if (company.positionStr == "" || (company.position != undefined && company.positionStr == company.position.address))
-                    company.positionStr = data.address;
-                company.position = data;
-                that.setData({
-                    company: company
-                });
+                    obj = {
+                        "company.position": data,
+                        "company.positionStr": data.address,
+                    };
+                that.setData(obj);
             }
         })
     },
     positionSave: function (e) {
-        let company = this.data.company;
-        company.positionStr = e.detail.value;
         this.setData({
-            company: company
+            "company.positionStr": e.detail.value
         });
     },
     useBindPhone: function () {
@@ -68,13 +71,58 @@ Page({
             console.log(response);
             that.setData({
                 type: 1,
-                phone: response.purePhoneNumber,
+                "user.phone": response.purePhoneNumber,
             });
         })
     },
     bindAgreeChange: function (e) {
         this.setData({
-            isAgree: !!e.detail.value.length
+            isAgree: !!e.detail.value.length,
         });
     },
+    uploadImages: function () {
+        let that = this,
+            images = that.data.images,
+            imageKeys = that.data.imageKeys;
+        this.setData({
+            deleteImage: false,
+        })
+        if (images.length > 2)
+            return false;
+        qiNiu.choseImg(3 - images.length, function (info) {
+            images.push(that.data.domain + info.key);
+            imageKeys.push(info.key);
+            that.setData({
+                images: images,
+                imageKeys: imageKeys
+            })
+        });
+    },
+    previewImage: function (e) {
+        let that = this,
+            index = e.currentTarget.dataset.id,
+            images = that.data.images;
+        wx.previewImage({
+            current: images[index], // 当前显示图片的http链接
+            urls: images // 需要预览的图片http链接列表
+        })
+    },
+    openDeleteImage: function (e) {
+        console.log(e);
+        this.setData({
+            deleteImage: true
+        })
+    },
+    deleteImage: function (e) {
+        let that = this,
+            index = e.currentTarget.dataset.id,
+            images = that.data.images,
+            imageKeys = that.data.imageKeys;
+        images.splice(index, 1);
+        imageKeys.splice(index, 1);
+        that.setData({
+            images: images,
+            imageKeys: imageKeys
+        })
+    }
 })
