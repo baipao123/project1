a16cfe9a56071e91a2166765468990045c883d70
huangchen <772965632@qq.com>
2018-04-27 16:09:33 +0800

    .

--- frontend/modules/part/controllers/CompanyController.php
+++ frontend/modules/part/controllers/CompanyController.php
@@ -38,7 +38,7 @@ class CompanyController extends \frontend\controllers\BaseController
         $company = $user->company;
         if (!$company || $company->status != Company::STATUS_PASS)
             return Tool::reJson(null, "您的企业资料未审核通过，暂时无法发布招聘信息", Tool::FAIL);
-        $res = Job::saveJob($_POST);
+        $res = Job::saveJob($_POST, $this->getPost("tmp", 0) > 0);
         if ($res == true)
             return Tool::reJson(1, "发布招聘信息成功");
         return Tool::reJson(null, $res === false ? "发布招聘信息失败，请稍后重试" : $res, Tool::FAIL);
@@ -52,7 +52,7 @@ class CompanyController extends \frontend\controllers\BaseController
         if (!$company || $company->status != Company::STATUS_PASS)
             return Tool::reJson(null, "您的企业资料未审核通过，暂时无法保存招聘信息", Tool::FAIL);
         $jid = $this->getPost("jid", 0);
-        $res = Job::saveJob($_POST, $jid);
+        $res = Job::saveJob($_POST, $this->getPost("tmp", 0) > 0, $jid);
         if ($res == true)
             return Tool::reJson(1, "修改招聘信息成功");
         return Tool::reJson(null, $res === false ? "修改招聘信息失败，请稍后重试" : $res, Tool::FAIL);
@@ -76,6 +76,26 @@ class CompanyController extends \frontend\controllers\BaseController
         $newJob->status = Job::TMP;
         $newJob->save();
         return Tool::reJson($newJob->info(), "复制招聘信息失败");
+    }
+
+    public function actionToggleJob() {
+        $jid = $this->getPost("jid", 0);
+        $job = Job::findOne($jid);
+        if (!$job || $job->uid != Yii::$app->user->id || $job->status == Job::DEL)
+            return Tool::reJson(null, "未找到原招聘信息", Tool::FAIL);
+        if ($job->status == Job::TMP)
+            return Tool::reJson(null, "草稿状态不能上架", Tool::FAIL);
+        $company = $job->company;
+        if (!$company || $company->status != Company::STATUS_PASS)
+            return Tool::reJson(null, "您的企业资料未审核通过，暂时无法操作招聘信息", Tool::FAIL);
+        $status = $this->getPost("status", 0);
+        if (!in_array($status, [Job::OFF, Job::ON]))
+            return Tool::reJson(null, "非法请求", Tool::FAIL);
+        $job->status = $status;
+        if ($job->save())
+            return Tool::reJson(1, "成功");
+        Yii::warning($job->errors, "保存Job失败");
+        return Tool::reJson(null, "失败", Tool::FAIL);
     }
 
     public function actionJobList() {
