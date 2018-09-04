38e6dc5b87277f82106ddc743c2fc78169616a02
baipao123 <huangchen@huangchendeMac-mini.local>
2018-09-04 22:09:02 +0800

    注册选学校

--- wxApp/pages/chose/user.js
+++ wxApp/pages/chose/user.js
@@ -9,11 +9,18 @@ Page({
         phoneText: '',
         isAgree: false,
         user:{},
+        schools: [],
+        school_value: 0,
+        school_year: 0,
+        schoolYears: [],
     },
     onLoad: function () {
         this.setData({
             user: app.globalData.user
         })
+        if (app.globalData.user)
+            this.getSchools(app.globalData.user.city_id, app.globalData.user.school_id)
+        this.getYears()
         wx.hideShareMenu()
     },
     bindAgreeChange: function (e) {
@@ -37,6 +44,8 @@ Page({
         let data = e.detail.value;
         let that = this;
         data.formId = e.detail.formId;
+        data.school_id = that.data.school_id
+        data.school_year = that.data.school_year
         request.post("part/user/join-user",data,function(res){
             app.globalData.user = res.user;
             app.toast("注册成功")
@@ -53,12 +62,65 @@ Page({
                 "user.cityStr": region.cityStr
             })
             app.resetRegion()
+            this.getSchools(region.cid)
         }
     },
     goSelectDistrict: function (e) {
         let that = this
         wx.navigateTo({
             url: "/pages/districtSelect/districtSelect?aid=" + that.data.user.area_id + "&cid=" + that.data.user.city_id
+        })
+    },
+    getSchools: function (city_id,school_id) {
+        let that = this
+        city_id = city_id == undefined ? 0 : city_id
+        school_id = school_id == undefined ? 0 : school_id
+        request.get("part/school/list", {city_id: city_id,school_id:school_id}, function (res) {
+            that.setData({
+                schools:res.schools,
+                school_value:res.index
+            })
+        })
+    },
+    getYears:function (year) {
+        let that = this,
+            thisYear = (new Date()).getFullYear(),
+            arr = [],
+            value = 0
+        arr.push("请选择学年")
+        if(year < thisYear - 4) {
+            arr.push(year)
+            value = 1
+        }
+        for (let index = thisYear - 4; index <= thisYear; index++) {
+            arr.push(index)
+            if(year == index)
+                value = index - thisYear + 4
+        }
+        that.setData({
+            schoolYears: arr,
+            school_year: value
+        })
+        console.log(arr)
+
+    },
+    choseSchool:function (e) {
+        let that = this,
+            index = e.detail.value,
+            school = that.data.schools[index],
+            school_id = school.id,
+            school_name = school.name
+        that.setData({
+            school_id:school_id,
+            school_name:school_id == 0 ? "" : school_name
+        })
+    },
+    choseSchoolYear:function (e) {
+        let that = this,
+            index = e.detail.value,
+            year = that.data.schoolYears[index]
+        that.setData({
+            school_year:index == 0 ? 0 : year
         })
     },
 })
